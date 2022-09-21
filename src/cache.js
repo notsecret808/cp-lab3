e2b7f9790b9edd7a2d59fbb10fc154004440371e
notsecret808 <thisisglodboy@gmail.com>
2022-09-21 03:02:42 +0300

    - implement Cache class - add test for Cache class

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,48 @@
-class Cache{
+export class Cache {
+	#cacheValuesMap = new Map();
+	#cacheRequestsLog = [];
 
+	addValueByKey({ key, value, requests = 1 }) {
+		if (!key) {
+			throw new Error("Key isn't valid");
+		}
+
+		this.#cacheValuesMap.set(key, { value, requests });
+	}
+
+	getValueByKey(key) {
+		if (!this.#cacheValuesMap.has(key)) {
+			return null;
+		}
+
+		const keyValue = this.#cacheValuesMap.get(key);
+		const updatedKeyValue = { ...keyValue, requests: keyValue.requests - 1 };
+
+		this.#cacheRequestsLog.unshift({ ...updatedKeyValue, key });
+
+		if (keyValue.requests == 0) {
+			this.#cacheValuesMap.delete(key);
+			return null;
+		}
+
+		this.#cacheValuesMap.set(updatedKeyValue);
+
+		return keyValue.value;
+	}
+
+	get getStat() {
+		return this.#cacheRequestsLog;
+	}
+
+	get getStatAsText() {
+		if (this.#cacheRequestsLog.length < 1) {
+			return '';
+		}
+
+		return this.#cacheRequestsLog
+			.map(({ key, value, requests }, index) => {
+				return `${ index + 1 }: key: ${ key }, value: ${ value }, requests: ${ requests };`;
+			})
+			.join('\r\n');
+	}
 }
-export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,104 @@
-import {Cache} from "../src/cache";
+import { Cache } from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
-});
+describe('Cache test', () => {
+	describe('Cache: addValueByKey tests', () => {
+		const cache = new Cache();
+
+		test('addValueByKey method should throw error', () => {
+			const testData = {
+				key: undefined,
+			};
+
+			expect(() => cache.addValueByKey(testData)).toThrowError(new Error("Key isn't valid"));
+		});
+
+		test('addValueByKey method should add key with value', () => {
+			const testData = {
+				key: 'foo1',
+				value: 728,
+				requests: 2
+			};
+
+			cache.addValueByKey(testData);
+
+			expect(cache.getValueByKey(testData.key)).toBe(testData.value);
+		});
+
+		test('addValueByKey method should set number of requests to 1 by default', () => {
+			const testData = {
+				key: 'foo2',
+				value: 2
+			};
+
+			cache.addValueByKey(testData);
+			cache.getValueByKey(testData.key);
+
+			expect(cache.getStat[0].requests).toBe(0);
+		});
+	});
+
+	describe('Cache: getValueByKey tests', () => {
+		const cache = new Cache();
+
+		test('getValueByKey method should return null by on non-existing key pass', () => {
+			expect(cache.getValueByKey('key')).toBeNull();
+		});
+
+		test('getValueByKey method should return null by on key with zero requests ', () => {
+			const testData = {
+				key: 'foo1',
+				value: 1,
+				requests: 0
+			};
+
+			cache.addValueByKey(testData);
+
+			expect(cache.getValueByKey(testData.key)).toBeNull();
+		});
+
+		test('getValueByKey method should return value on valid key pass', () => {
+			const testData = {
+				key: 'foo2',
+				value: 852
+			};
+
+			cache.addValueByKey(testData);
+
+			expect(cache.getValueByKey(testData.key)).toBe(testData.value);
+		});
+	});
+
+	describe('Cache: getStatAsText tests', () => {
+		const cache = new Cache();
+		
+		test('getStat method should return empty string on empty requests log', () => {
+			expect(cache.getStatAsText).toBe('');
+		});
+
+		test('getStat method should return target text on non-empty requests log', () => {
+			const testData1 = {
+				key: 'foo1',
+				value: 111,
+				requests: 2
+			};
+
+			const testData2 = {
+				key: 'foo2',
+				value: 222,
+				requests: 2
+			};
+
+			const targetTextLinesArray = [
+				`1: key: ${ testData2.key }, value: ${ testData2.value }, requests: ${ testData2.requests - 1 };`,
+				`2: key: ${ testData1.key }, value: ${ testData1.value }, requests: ${ testData1.requests - 1 };`
+			];
+
+			cache.addValueByKey(testData1);
+			cache.getValueByKey(testData1.key);
+			cache.addValueByKey(testData2);
+			cache.getValueByKey(testData2.key);
+
+			expect(cache.getStatAsText).toBe(targetTextLinesArray.join('\r\n'));
+		});
+	});
+});;
